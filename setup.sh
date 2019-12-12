6bb96e6a60bbf04482aee7c69c3103884f221c07
Amal Santhosh <42336393+Amalsanthoshp@users.noreply.github.com>
2019-12-12 13:12:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
+mkdir /workspace/go
 mkdir /workspace/go/src
-mkdir /workspace/go/github.com/
+mkdir /workspace/go/src/github.com/
 export GOPATH=/workspace/go
 cd $GOPATH/src/github.com/
 mkdir -p AlphaDinoRC
 cd AlphaDinoRC
 git clone https://github.com/AlphaDinoRC/ixxo_dappbox.git
-cd $GOPATH/src/github.com/AlphaDinoRC/ixxo_dappbox
-sh dappbox_build_linux.sh 
+cd /workspace/test/
+./dappbox_build_linux.sh 
